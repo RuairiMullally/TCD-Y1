921bfdcd305eaa694aad7e21509a16482981def4
RuairiMullally <123960178+RuairiMullally@users.noreply.github.com>
2023-03-31 11:52:12 +0100

    Add files via upload

---
+++ Wk9P1 - Graph Producer.cpp
@@ -0,0 +1,86 @@
+//Ruairi Mullally 22336002
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//function declarations
+void readInData(int integers[], int SIZE);
+void initialise(char yAxisValues[], int SIZE);
+
+void convertArrays(const int integers[], char yAxisValues[], int SIZE, int iteration);
+void printArray(char yAxisValues[], int SIZE, int iteration);
+
+void printGraph(const int integers[], char yAxisValues[], int SIZE, int iteration);
+
+void scaleBars(int N);
+
+const int SIZE = 70; // determines max 'x' value for length of the scale bars & arrays
+const int HEIGHT = 20; //determines max 'y' value, contols loops
+
+int main() {
+  int integers[SIZE], iteration = HEIGHT;
+  char yAxisValues[SIZE];
+
+  readInData(integers, SIZE); //read in integer values with corresponding x position (array slot)
+  initialise(yAxisValues, SIZE); //initialises yAxisValues with blanks
+
+  printGraph(integers, yAxisValues, SIZE, iteration);
+  scaleBars(SIZE/10);
+}
+
+
+void readInData(int integers[], int SIZE){ //reads in the list of integers to be graphed
+  for(int i=0; i<SIZE; i++)
+    cin >> integers[i];
+}
+
+
+void initialise(char yAxisValues[], int SIZE){//initialises 'y' axis array to blank spaces
+  for(int i=0; i<SIZE; i++)
+    yAxisValues[i]=' ';
+}
+
+
+
+void convertArrays(const int integers[], char yAxisValues[], int SIZE, int iteration){//if there is a value at that level iteration for some x value, the yAxisValues receives an '*' in that position
+  for(int i=0; i<SIZE; i++){
+    if(integers[i] == iteration){
+      yAxisValues[i] = '*';
+    }
+  }
+}
+
+
+void printArray(char yAxisValues[], int SIZE, int iteration){ //prints the vertical bars forming the graph
+
+    cout << setw(2) << iteration << setw(3)  << "|";
+
+    for(int n=0; n<SIZE; n++){
+      cout << yAxisValues[n];
+    }
+    cout << endl;
+}
+
+
+void printGraph(const int integers[], char yAxisValues[], int SIZE, int iteration){
+
+    for(int p=HEIGHT; p>0; p--){
+        convertArrays(integers, yAxisValues, SIZE, iteration);
+        printArray(yAxisValues, SIZE, iteration);
+        iteration --;
+  }
+}
+
+
+
+void scaleBars(int N) { //prints out scale bars at bottom of graph
+  cout << "----|";
+  for (int n = 1; n <= N; n++)
+    cout << "----+----|";
+}
+
+
+
+
+
---
+++ Wk9P2 - MCQ Marker.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+void readInData(char answers[], int SIZE);
+int questionScorer(const char answers[], char attempt[], int SIZE);
+int examScorer(const char answers[], char attempt[], int SIZE);
+int scoreAttempts(const char answers[], char attempt[], int SIZE);
+
+int main() {
+  const int SIZE = 20; //length of MCQ test
+  char answers[SIZE], attempt[SIZE];
+  int score = 0;
+  int studentID;
+
+
+  readInData(answers, SIZE);//read in answers to reference
+  cin >> studentID;
+
+
+  while(studentID >0){//while there is a valid studentID, the program will mark tests and output the result
+
+    score = scoreAttempts(answers, attempt, SIZE);
+    cout << studentID <<": "<<score<<endl;
+    cin >> studentID;
+  }
+
+
+}
+
+void readInData(char answers[], int SIZE){//reads in answers
+  for(int i=0; i<SIZE; i++)
+    cin >> answers[i];
+}
+
+int questionScorer(const char answers[], char attempt[], int SIZE, int question_num){//compares array to answer array and return te mark for that q
+  if(attempt[question_num] == 'x')
+    return 0;
+  else if(attempt[question_num] == answers[question_num])
+    return 2;
+  return -1;
+}
+
+int examScorer(const char answers[], char attempt[], int SIZE){//returns the sum of marks for all questions
+  int mark = 0;
+  for(int i=0; i<SIZE; i++){
+    mark += questionScorer(answers, attempt, SIZE, i);
+  }
+  return mark;
+}
+
+int scoreAttempts(const char answers[], char attempt[], int SIZE){//reads in an attempt and returns the score for that attempt
+  readInData(attempt, SIZE);
+  int score = examScorer(answers, attempt, SIZE);
+  return score;
+
+}
